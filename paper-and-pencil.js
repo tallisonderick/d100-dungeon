6e7c53db48db23bad5c540952b2931161c919bd5
Tallison Derick <tallison.cp2@gmail.com>
2018-11-14 20:49:55 -0200

    Tabela monstros construída com knockout

--- paper-and-pencil.js
+++ paper-and-pencil.js
@@ -1,14 +1,31 @@
+function AppViewModel() {
+	this.monstros = ko.observableArray([
+		{ nome: "GIANT RAT", modificadorDano: -2, valorAtaque: 25 },
+		{ nome: "GIANT BAT", modificadorDano: -3, valorAtaque: 25 },
+		{ nome: "GIANT ANT", modificadorDano: -2, valorAtaque: 20 },
+		{ nome: "GIANT SPIDER", modificadorDano: -1, valorAtaque: 30 },
+		{ nome: "GOBALOTES", modificadorDano: -2, valorAtaque: 25 },
+		{ nome: "GOBLIN", modificadorDano: -1, valorAtaque: 25 },
+		{ nome: "GOBLIN ARCHER", modificadorDano: -1, valorAtaque: 25 },
+		{ nome: "GOBLIN WARLOCK", modificadorDano: -1, valorAtaque: 30 },
+		{ nome: "BEAR", modificadorDano: 0, valorAtaque: 40 },
+		{ nome: "RAT MAN", modificadorDano: 0, valorAtaque: 30 }
+	]);
+}
+
+ko.applyBindings(new AppViewModel());
+
 var monstros = [
-	{nome:"GIANT RAT", modificadorDano: -2, valorAtaque: 25},
-	{nome:"GIANT BAT", modificadorDano: -3, valorAtaque: 25},
-	{nome:"GIANT ANT", modificadorDano: -2, valorAtaque: 20},
-	{nome:"GIANT SPIDER", modificadorDano: -1, valorAtaque: 30},
-	{nome:"GOBALOTES", modificadorDano: -2, valorAtaque: 25},
-	{nome:"GOBLIN", modificadorDano: -1, valorAtaque: 25},
-	{nome:"GOBLIN ARCHER", modificadorDano: -1, valorAtaque: 25},
-	{nome:"GOBLIN WARLOCK", modificadorDano: -1, valorAtaque: 30},
-	{nome:"BEAR", modificadorDano: 0, valorAtaque: 40},
-	{nome:"RAT MAN", modificadorDano: 0, valorAtaque: 30}
+	{ nome: "GIANT RAT", modificadorDano: -2, valorAtaque: 25 },
+	{ nome: "GIANT BAT", modificadorDano: -3, valorAtaque: 25 },
+	{ nome: "GIANT ANT", modificadorDano: -2, valorAtaque: 20 },
+	{ nome: "GIANT SPIDER", modificadorDano: -1, valorAtaque: 30 },
+	{ nome: "GOBALOTES", modificadorDano: -2, valorAtaque: 25 },
+	{ nome: "GOBLIN", modificadorDano: -1, valorAtaque: 25 },
+	{ nome: "GOBLIN ARCHER", modificadorDano: -1, valorAtaque: 25 },
+	{ nome: "GOBLIN WARLOCK", modificadorDano: -1, valorAtaque: 30 },
+	{ nome: "BEAR", modificadorDano: 0, valorAtaque: 40 },
+	{ nome: "RAT MAN", modificadorDano: 0, valorAtaque: 30 }
 ];
 
 var model = {
@@ -31,99 +48,80 @@ function atualizarTela(model) {
 	document.getElementById('monstroNome').value = model.monstroNome;
 	document.getElementById('monstroModificadorDano').value = model.monstroModificadorDano;
 	document.getElementById('monstroValorAtaque').value = model.monstroValorAtaque;
-	
+
 	criarTabelaMonstros();
 }
-	
+
 function rolarDado(faces) {
 	var valorDado = Math.floor((Math.random() * faces) + 1);
 	return valorDado;
 }
 
 function rolarEncontro() {
-	var dadoMonstro = rolarDado(10) - 1;
-	
-	model.monstroDado = dadoMonstro + 1;
-	model.monstroNome = monstros[dadoMonstro].nome;
-	model.monstroModificadorDano = monstros[dadoMonstro].modificadorDano;;
-	model.monstroValorAtaque = monstros[dadoMonstro].valorAtaque;
+	var dadoMonstro = rolarDado(10);
+	var pos = dadoMonstro - 1;
+
+	model.monstroDado = dadoMonstro;
+	model.monstroNome = monstros[pos].nome;
+	model.monstroModificadorDano = monstros[pos].modificadorDano;;
+	model.monstroValorAtaque = monstros[pos].valorAtaque;
 
 	var d10Imagem = "<img class='d10' src='faceDados/d10-" + model.monstroDado + ".png'>";
 	document.getElementById('d10').innerHTML = d10Imagem;
-	
+
 	atualizarTela(model);
 }
 
 function rolarAv() {
-	var d100Imagem = "";
 	var dadoDezena = (rolarDado(10) - 1) * 10;
 	var dadoUnidade = (rolarDado(10) - 1);
 	var somaDado = dadoDezena + dadoUnidade;
-	
-	if(somaDado == 0) {
+
+	if (somaDado == 0) {
 		model.dadoMonstroValorAtaque = 100;
 	} else {
 		model.dadoMonstroValorAtaque = somaDado;
 	}
-	
-	if(dadoDezena == 0) {
-		d100Imagem = "<img class='d10' src='faceDados/d10-00.png'>";
+
+	if (dadoDezena == 0) {
+		var d100Imagem = "<img class='d10' src='faceDados/d10-00.png'>";
 	} else {
-		d100Imagem = "<img class='d10' src='faceDados/d10-" + dadoDezena + ".png'>";
+		var d100Imagem = "<img class='d10' src='faceDados/d10-" + dadoDezena + ".png'>";
 	}
-	
+
 	d100Imagem += "<img class='d10' src='faceDados/d10-" + dadoUnidade + ".png'>";
 	document.getElementById('d100').innerHTML = d100Imagem;
-	
+
 	atualizarTela(model);
 }
 
 function rolarDano() {
 	model.dadoDano = rolarDado(6);
-	
+
 	var danoCalculado = calcularDano(model.monstroModificadorDano);
-	
-	if(model.dadoMonstroValorAtaque <= model.monstroValorAtaque) {
+
+	if (model.dadoMonstroValorAtaque <= model.monstroValorAtaque) {
 		model.dano = danoCalculado;
 	} else {
 		model.dano = 0;
 	}
-	
+
 	var d6Imagem = "<img class='d6' src='faceDados/d6-" + model.dadoDano + ".png'>";
 	document.getElementById('d6').innerHTML = d6Imagem;
-	
+
 	atualizarTela(model);
 }
 
 function calcularDano(modificadorDano) {
 	var dano = model.dadoDano + modificadorDano;
-	
-	if(dano < 0){
-		return 0;
-	}
-	
+
+	if (dano < 0) return 0;
+
 	return dano;
 }
 
 function sofrerDano() {
-	var novoHp = model.hp - model.dano;
-	
-	model.hp = novoHp;
-	
-	atualizarTela(model);
-}
+	model.hp = model.hp - model.dano;
 
-function criarTabelaMonstros(){
-	var tabela = "<tr><th>D10</th><th>MONSTRO ENCONTRADO</th><th>AV</th><th>DMG</th></tr>";
-	
-	for(var i = 0; i < monstros.length; i++) {
-		tabela += "<tr>";
-		tabela += "<td>" + (i + 1) + "</td>";
-        tabela += "<td>" + monstros[i].nome + "</td>";
-		tabela += "<td>" + monstros[i].valorAtaque + "</td>";
-		tabela += "<td>" + monstros[i].modificadorDano + "</td>";
-		tabela += "</tr>";
-	}
-	
-	document.getElementById('tabelaMonstros').innerHTML = tabela;
+	atualizarTela(model);
 }
