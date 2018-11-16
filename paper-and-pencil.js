5c2b52e48bd52ea05f5b54fccd696e9542093dd3
Tallison Derick <tallison.cp2@gmail.com>
2018-11-16 21:52:57 -0200

    Começando uso de objetos

--- paper-and-pencil.js
+++ paper-and-pencil.js
@@ -1,32 +1,67 @@
 function AppViewModel() {
-	this.monstros = ko.observableArray([
-		{ nome: "GIANT RAT", modificadorDano: -2, valorAtaque: 25 },
-		{ nome: "GIANT BAT", modificadorDano: -3, valorAtaque: 25 },
-		{ nome: "GIANT ANT", modificadorDano: -2, valorAtaque: 20 },
-		{ nome: "GIANT SPIDER", modificadorDano: -1, valorAtaque: 30 },
-		{ nome: "GOBALOTES", modificadorDano: -2, valorAtaque: 25 },
-		{ nome: "GOBLIN", modificadorDano: -1, valorAtaque: 25 },
-		{ nome: "GOBLIN ARCHER", modificadorDano: -1, valorAtaque: 25 },
-		{ nome: "GOBLIN WARLOCK", modificadorDano: -1, valorAtaque: 30 },
-		{ nome: "BEAR", modificadorDano: 0, valorAtaque: 40 },
-		{ nome: "RAT MAN", modificadorDano: 0, valorAtaque: 30 }
+	this.encontro = ko.observableArray([
+		{ min: 1, max: 10, nome: "GIANT RAT", valorAtaque: 25, modificadorDano: -2, hp: 3 },
+		{ min: 11, max: 20, nome: "GIANT BAT", valorAtaque: 25, modificadorDano: -3, hp: 2 },
+		{ min: 21, max: 25, nome: "GIANT ANT", valorAtaque: 20, modificadorDano: -2, hp: 3 },
+		{ min: 26, max: 30, nome: "GIANT SPIDER", valorAtaque: 30, modificadorDano: -1, hp: 6 },
+		{ min: 31, max: 33, nome: "GOBALOTE", valorAtaque: 25, modificadorDano: -2, hp: 3 },
+		{ min: 34, max: 37, nome: "GOBLIN", valorAtaque: 25, modificadorDano: -1, hp: 2 },
+		{ min: 38, max: 41, nome: "GOBLIN ARCHER", valorAtaque: 25, modificadorDano: -1, hp: 2 },
+		{ min: 42, max: 42, nome: "GOBLIN WARLOCK", valorAtaque: 30, modificadorDano: -1, hp: 7 },
+		{ min: 43, max: 44, nome: "BEAR", valorAtaque: 40, modificadorDano: 0, hp: 10 },
+		{ min: 45, max: 46, nome: "RAT MAN", valorAtaque: 30, modificadorDano: 0, hp: 3 }
 	]);
 }
 
 ko.applyBindings(new AppViewModel());
 
-var monstros = [
-	{ nome: "GIANT RAT", modificadorDano: -2, valorAtaque: 25 },
-	{ nome: "GIANT BAT", modificadorDano: -3, valorAtaque: 25 },
-	{ nome: "GIANT ANT", modificadorDano: -2, valorAtaque: 20 },
-	{ nome: "GIANT SPIDER", modificadorDano: -1, valorAtaque: 30 },
-	{ nome: "GOBALOTES", modificadorDano: -2, valorAtaque: 25 },
-	{ nome: "GOBLIN", modificadorDano: -1, valorAtaque: 25 },
-	{ nome: "GOBLIN ARCHER", modificadorDano: -1, valorAtaque: 25 },
-	{ nome: "GOBLIN WARLOCK", modificadorDano: -1, valorAtaque: 30 },
-	{ nome: "BEAR", modificadorDano: 0, valorAtaque: 40 },
-	{ nome: "RAT MAN", modificadorDano: 0, valorAtaque: 30 }
+var encontro = [
+	{ min: 1, max: 10, nome: "GIANT RAT", valorAtaque: 25, modificadorDano: -2, hp: 3 },
+	{ min: 11, max: 20, nome: "GIANT BAT", valorAtaque: 25, modificadorDano: -3, hp: 2 },
+	{ min: 21, max: 25, nome: "GIANT ANT", valorAtaque: 20, modificadorDano: -2, hp: 3 },
+	{ min: 26, max: 30, nome: "GIANT SPIDER", valorAtaque: 30, modificadorDano: -1, hp: 6 },
+	{ min: 31, max: 33, nome: "GOBALOTE", valorAtaque: 25, modificadorDano: -2, hp: 3 },
+	{ min: 34, max: 37, nome: "GOBLIN", valorAtaque: 25, modificadorDano: -1, hp: 2 },
+	{ min: 38, max: 41, nome: "GOBLIN ARCHER", valorAtaque: 25, modificadorDano: -1, hp: 2 },
+	{ min: 42, max: 42, nome: "GOBLIN WARLOCK", valorAtaque: 30, modificadorDano: -1, hp: 7 },
+	{ min: 43, max: 44, nome: "BEAR", valorAtaque: 40, modificadorDano: 0, hp: 10 },
+	{ min: 45, max: 46, nome: "RAT MAN", valorAtaque: 30, modificadorDano: 0, hp: 3 }
 ];
+
+function MonstroEncontrado(encontro) {
+	var self = this;
+
+	self.hp = encontro.hp;
+	self.encontro = encontro;
+}
+
+function Personagem(hp, forca) {
+	var self = this;
+
+	self.hp = hp;
+	self.forca = forca;
+}
+
+function LocalAtingido() {
+	var self = this;
+
+	self.dadoDeLocal = '';
+	self.localAtingido = '';
+	self.modificadorDeDano = '';
+}
+
+function ReacaoDoMonstro() {
+	var self = this;
+
+	self.dadoDeReacao = '';
+	self.reacaoDoMonstro = '';
+}
+
+function Combate() {
+
+}
+
+window.Combate = new Combate();
 
 var model = {
 	hp: 20,
@@ -60,9 +95,9 @@ function rolarEncontro() {
 	var pos = dadoMonstro - 1;
 
 	model.monstroDado = dadoMonstro;
-	model.monstroNome = monstros[pos].nome;
-	model.monstroModificadorDano = monstros[pos].modificadorDano;;
-	model.monstroValorAtaque = monstros[pos].valorAtaque;
+	model.monstroNome = encontro[pos].nome;
+	model.monstroModificadorDano = encontro[pos].modificadorDano;;
+	model.monstroValorAtaque = encontro[pos].valorAtaque;
 
 	var d10Imagem = "<img class='d10' src='faceDados/d10-" + model.monstroDado + ".png'>";
 	document.getElementById('d10').innerHTML = d10Imagem;
